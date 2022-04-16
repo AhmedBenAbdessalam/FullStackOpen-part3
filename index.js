415a7371d6bfeff0efa4ba5e285634028250f44a
Ahmed Ben Abdessalam <ahmed.ben.abdessalam93@gmail.com>
2022-04-16 15:12:10 +0100

    step 2

--- index.js
+++ index.js
@@ -29,6 +29,13 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  const nbPpl = persons.length
+  response.send(
+    `<p>Phonebook has info for ${nbPpl} people</p><p>${new Date()}</p>`
+  )
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
