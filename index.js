c63590ef4350402e63f9e58b579a23edb84ccb15
Ahmed Ben Abdessalam <ahmed.ben.abdessalam93@gmail.com>
2022-04-17 18:05:41 +0100

    uibuild

--- index.js
+++ index.js
@@ -20,33 +20,6 @@ app.use(morgan((tokens, req, res) =>
   ].join(' ')
 ))
 
-
-
-// let persons = [
-//   {
-//     "id": 1,
-//     "name": "Arto Hellas",
-//     "number": "040-123456"
-//   },
-//   {
-//     "id": 2,
-//     "name": "Ada Lovelace",
-//     "number": "39-44-5323523"
-//   },
-//   {
-//     "id": 3,
-//     "name": "Dan Abramov",
-//     "number": "12-43-234345"
-//   },
-//   {
-//     "id": 4,
-//     "name": "Mary Poppendieck",
-//     "number": "39-23-6423122"
-//   }
-// ]
-
-
-
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => response.json(persons))
 
@@ -58,13 +31,19 @@ app.get('/info', (request, response) => {
     `<p>Phonebook has info for ${nbPpl} people</p><p>${new Date()}</p>`
   )
 })
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => response.json(person))
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id).then(person => {
+    if (person) {
+      response.json(person)
+    }
+    else {
+      response.status(404).end()
+    }
+  }).catch(error => next(error))
 })
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
+  Person.findByIdAndDelete(request.params.id).then(result => response.status(204).end()).catch(error => next(error))
+
 })
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -88,6 +67,18 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
