0b02000c852efe02d2c0f5154ae8352687b5dd2e
Ahmed Ben Abdessalam <ahmed.ben.abdessalam93@gmail.com>
2022-04-17 17:42:03 +0100

    step2 db

--- index.js
+++ index.js
@@ -59,14 +59,7 @@ app.get('/info', (request, response) => {
   )
 })
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    response.json(person)
-  }
-  else {
-    response.status(404).end()
-  }
+  Person.findById(request.params.id).then(person => response.json(person))
 })
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
@@ -75,23 +68,19 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 app.post('/api/persons', (request, response) => {
   const body = request.body
+  console.log(body)
   if (!body.name) {
     response.status(400).json({ error: "name missing" })
   }
   else if (!body.number) {
     response.status(400).json({ error: "number missing" })
   }
-  else if (persons.find(person => person.name === body.name)) {
-    response.status(400).json({ error: "name already exist" })
-  }
   else {
-    const person = {
-      "id": Math.floor(Math.random() * 100000),
+    const person = Person({
       "name": body.name,
       "number": body.number
-    }
-    persons = persons.concat(person)
-    response.json(body)
+    })
+    person.save().then(savedPerson => response.json(savedPerson))
   }
 })
 
