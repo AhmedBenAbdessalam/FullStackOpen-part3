bdb8246574ab3ea7e277daf95a0d6728def635b5
Ahmed Ben Abdessalam <ahmed.ben.abdessalam93@gmail.com>
2022-04-17 18:18:04 +0100

    uibuild

--- index.js
+++ index.js
@@ -26,10 +26,12 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-  const nbPpl = persons.length
-  response.send(
-    `<p>Phonebook has info for ${nbPpl} people</p><p>${new Date()}</p>`
-  )
+  Person.find({}).then(persons => {
+    response.send(
+      `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
+    )
+  })
+
 })
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person => {
@@ -41,9 +43,20 @@ app.get('/api/persons/:id', (request, response, next) => {
     }
   }).catch(error => next(error))
 })
-app.delete('/api/persons/:id', (request, response) => {
-  Person.findByIdAndDelete(request.params.id).then(result => response.status(204).end()).catch(error => next(error))
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  Person.findByIdAndUpdate(request.params.id, body, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => response.status(204).end())
+    .catch(error => next(error))
 })
 app.post('/api/persons', (request, response) => {
   const body = request.body
